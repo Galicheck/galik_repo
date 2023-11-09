b9c0b7a6affc9e135522cd09cf69bf8c614af6cc
jgalikowski <kubagalikowskipokego@gmail.com>
2023-11-09 13:33:54 +0100

    OOP 09.11

--- OOP 31.10.py
+++ OOP 31.10.py
@@ -15,22 +15,36 @@
 
 # %%
 import random
-import matplotlib.pyplot
-
+import matplotlib.pyplot as plt
 
 # %%
+SIGMA = 0.02
 class Creature:
-    p_death = 0.2
-    p_reproduction = 0.2
     alive = True
     
+    def __init__(self, p_death=0.2, p_reproduction=0.2) :
+        self.p_death = p_death
+        self.p_reproduction = p_reproduction
+        
     def natural_selection(self):
         if random.random() <= self.p_death:
             self.alive = False
             
     def reproduce(self):
         if random.random() <= self.p_reproduction:
-            return Creature()
+            return Creature(self.p_death + random.normalvariate(sigma=SIGMA ), self.p_reproduction + random.normalvariate(sigma= SIGMA))
+    
+    @property
+    def p_death(self):
+        return self._p_death
+    
+    @p_death.setter
+    def p_death(self,value):
+        if value < 0:
+            self._p_death = 0.0
+        else:
+            self._p_death = value
+    
     
 class Population:
       
@@ -47,7 +61,7 @@ class Population:
             
     def reproduce(self):
         new_creatures = {creature.reproduce() for creature in self.specimen}
-        new_creatures -= None
+        new_creatures -= {None}
         self.specimen|= new_creatures
        
     def simulate(self, generations):
@@ -55,17 +69,41 @@ class Population:
             self.history.append(self.count_alive())
             self.natural_selection()
             self.reproduce()
-    
+        
+    def plot_history(self):
+        plt.plot(self.history)
+        
+    def plot_attribute(self,attribute='p_death'):
+        values = [getattr(creature,attribute) for creature in self.specimen if creature.alive]
+        plt.hist(values, density = True)
+        plt.title("p_death")
+        
 
 
 # %%
 population= Population(3000)
 
 # %%
-population.natural_selection()
+population.plot_attribute()
+
+# %%
+population.simulate(50)
+
+# %%
+population.plot_attribute()
+
+# %%
+population.plot_attribute(attribute='p_reproduction')
+
+# %%
 population.count_alive()
 
 # %%
-creature = Creature ()
+stwór = Creature()
+stwór.p_death = -1
+stwór.p_death
 
 # %%
+stwór = Creature()
+stwór._p_death = -1
+stwór.p_death
