22f92be38782df1c5fd922ade515745d4a3117d7
jgalikowski <kubagalikowskipokego@gmail.com>
2023-12-14 13:26:21 +0100

    twilio 14.12

--- twilio.py
+++ twilio.py
@@ -13,3 +13,17 @@ for url in url_list:
         print(f'Other error occured: {err}')
     else:
         print("success")
+
+
+url = 'https://console.twilio.com/us1/develop/sms/try-it-out/send-an-sms'
+client_id = AC29a245d297cdb258090968f49a3032d8
+client_secret = dcab90574c1b1d73ff5616a0eaefdcd1
+
+response = requests.get(url, auth=(client_id,client_secret))
+
+data = {'To': '+48515417186',
+        'From': '+13344543966',
+        'Body': 'Nie mam pomysłu'}
+response = requests.get(url, auth=(client_id,client_secret), data= data)
+
+response.status_code
